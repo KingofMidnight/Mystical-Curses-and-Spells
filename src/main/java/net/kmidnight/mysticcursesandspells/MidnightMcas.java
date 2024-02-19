a02a05a966b71bb92ee1555fd1c868216354739c
KingofMidnight <jemeritgonzalez05@gmail.com>
2024-02-18 20:04:49 -0400

    Update

--- src/main/java/net/kmidnight/mysticcursesandspells/MidnightMcas.java
+++ src/main/java/net/kmidnight/mysticcursesandspells/MidnightMcas.java
@@ -1,6 +1,8 @@
 package net.kmidnight.mysticcursesandspells;
 
 import com.mojang.logging.LogUtils;
+import net.kmidnight.mysticcursesandspells.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -28,6 +30,8 @@ public class MidnightMcas
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -46,6 +50,9 @@ public class MidnightMcas
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.MCASMAGICBOOK);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/kmidnight/mysticcursesandspells/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.kmidnight.mysticcursesandspells.item;
+
+import net.kmidnight.mysticcursesandspells.MidnightMcas;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MidnightMcas.MOD_ID);
+
+    public static final RegistryObject<Item> MCASMAGICBOOK = ITEMS.register("mcasmagicbook",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
