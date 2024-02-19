dfa9a866c663d97f97acedfb974e9fc64eb11999
KingofMidnight <jemeritgonzalez05@gmail.com>
2024-02-18 22:27:04 -0400

    Update

--- src/main/java/net/kmidnight/mysticcursesandspells/command/McasCommand.java
+++ src/main/java/net/kmidnight/mysticcursesandspells/command/McasCommand.java
@@ -3,22 +3,42 @@ package net.kmidnight.mysticcursesandspells.command;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
 
 public class McasCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("mcas")
-                .then(Commands.literal("curse"))
-                        .then(Commands.literal("list"))
-                        .then(Commands.literal("grant"))
-                        .then(Commands.literal("removal"))
-                .then(Commands.literal("spell"))
-                        .then(Commands.literal("list"))
-                        .then(Commands.literal("grant"))
-                        .then(Commands.literal("removal"))
-                .then(Commands.literal("mana-type"))
-                        .then(Commands.literal("list"))
-                        .then(Commands.literal("grant"))
-                        .then(Commands.literal("removal"))
+            .requires((source) -> source.hasPermission(2))
+            .then(Commands.literal("curse")
+                .then(Commands.argument("target", EntityArgument.players())
+                    .then(Commands.literal("list")
+                    )
+                    .then(Commands.literal("grant")
+                    )
+                    .then(Commands.literal("removal")
+                    )
+                )
+            )
+            .then(Commands.literal("spell")
+                .then(Commands.argument("target", EntityArgument.players())
+                    .then(Commands.literal("list")
+                    )
+                    .then(Commands.literal("grant")
+                    )
+                    .then(Commands.literal("removal")
+                    )
+                )
+            )
+            .then(Commands.literal("mana-type")
+                .then(Commands.argument("target", EntityArgument.players())
+                    .then(Commands.literal("has")
+                    )
+                    .then(Commands.literal("grant")
+                    )
+                    .then(Commands.literal("removal")
+                    )
+                )
+            )
         );
     }
 }
