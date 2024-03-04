93edf81ecfaab921de1a57475ea214e71d129dd3
KingofMidnight <57202402+kingofmidnight@users.noreply.github.com>
2024-03-03 20:03:53 -0400

    update

--- src/main/java/net/kmidnight/mysticalcursesandspells/curse/CurseManager.java
+++ src/main/java/net/kmidnight/mysticalcursesandspells/curse/CurseManager.java
@@ -1,4 +1,24 @@
 package net.kmidnight.mysticalcursesandspells.curse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CurseManager {
+    public List<Curse> curses;
+
+    public CurseManager() {
+        curses = new ArrayList<>();
+    }
+
+    public void addCurse(Curse curse) {
+        curses.add(curse);
+    }
+
+    public void removeCurse(Curse curse) {
+        curses.remove(curse);
+    }
+
+    public List<Curse> getAllCurses() {
+        return curses;
+    }
 }
