6a8ef88924bcf44f37322304b08703c218eff116
KingofMidnight <jemeritgonzalez05@gmail.com>
2024-03-09 15:27:32 -0400

    little bit more on the curses

--- src/main/java/net/kmidnight/mysticalcursesandspells/curse/Curse.java
+++ src/main/java/net/kmidnight/mysticalcursesandspells/curse/Curse.java
@@ -3,9 +3,12 @@ package net.kmidnight.mysticalcursesandspells.curse;
 import net.kmidnight.mysticalcursesandspells.MidnightMcas;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.RegistryBuilder;
+import net.minecraftforge.registries.RegistryObject;
+import net.kmidnight.mysticalcursesandspells.curse.curses.*;
 
 import java.util.function.Supplier;
 
@@ -14,6 +17,12 @@ public class Curse {
     public static final DeferredRegister<Curse> CURSE = DeferredRegister.create(new ResourceLocation(MidnightMcas.MOD_ID, "curse"), MidnightMcas.MOD_ID);
 
     public static final Supplier<IForgeRegistry<Curse>> REGISTRY = CURSE.makeRegistry(RegistryBuilder::new);
+
+    public static final RegistryObject<Curse> LIFEBANE = CURSE.register("lifebane", () -> new Lifebane());
+
+    public static void init() {
+        CURSE.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
 
     public void applyCurse(Player player) {
     }
--- src/main/java/net/kmidnight/mysticalcursesandspells/curse/curses/Lifebane.java
+++ src/main/java/net/kmidnight/mysticalcursesandspells/curse/curses/Lifebane.java
@@ -1,8 +1,9 @@
 package net.kmidnight.mysticalcursesandspells.curse.curses;
 
+import net.kmidnight.mysticalcursesandspells.curse.Curse;
 import net.minecraft.world.entity.player.Player;
 
-public class lifebane {
+public class Lifebane extends Curse {
     public void applyCurse(Player player) {
         int currentHealth = (int) player.getHealth();
         int reducedHealth = currentHealth / 2;
