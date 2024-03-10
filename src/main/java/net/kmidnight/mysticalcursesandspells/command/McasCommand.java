d7b19c85864465098fc5e96ea0ff7cc398273da8
Taxolotl <112824633+Taxolotl@users.noreply.github.com>
2024-03-10 14:12:05 -0400

    Update McasCommand.java

--- src/main/java/net/kmidnight/mysticalcursesandspells/command/McasCommand.java
+++ src/main/java/net/kmidnight/mysticalcursesandspells/command/McasCommand.java
@@ -1,12 +1,17 @@
-package net.kmidnight.mysticalcursesandspells.command;
+package net.kmidnight.mysticalcursesandspells.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
-import net.kmidnight.mysticalcursesandspells.curse.Curse;
-import net.kmidnight.mysticalcursesandspells.curse.CurseManager;
+import net.kmidnight.mysticalcursesandspells.curses.Curses;
+import net.kmidnight.mysticalcursesandspells.curses.custom.AbstractCurse;
+import net.kmidnight.mysticalcursesandspells.curses.CurseManager;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 public class McasCommand {
@@ -15,39 +20,42 @@ public class McasCommand {
             .requires((source) -> source.hasPermission(2))
             .then(Commands.literal("curse")
                 .then(Commands.argument("target", EntityArgument.players())
-                    .then(Commands.literal("list")
-                    )
-                    .then(Commands.literal("grant")
-                    )
-                    .then(Commands.literal("removal")
-                    )
+                    .then(Commands.literal("list"))
+                    .then(Commands.literal("grant").executes(pCommandSourceStack -> {
+                        return cast(pCommandSourceStack.getSource(), EntityArgument.getPlayers(pCommandSourceStack, "target"));
+                    }))
+                    .then(Commands.literal("removal"))
                 )
             )
             .then(Commands.literal("spell")
                 .then(Commands.argument("target", EntityArgument.players())
-                    .then(Commands.literal("list")
-                    )
-                    .then(Commands.literal("grant")
-                    )
-                    .then(Commands.literal("removal")
-                    )
+                    .then(Commands.literal("list"))
+                    .then(Commands.literal("grant"))
+                    .then(Commands.literal("removal"))
                 )
             )
             .then(Commands.literal("mana-type")
                 .then(Commands.argument("target", EntityArgument.players())
-                    .then(Commands.literal("has")
-                    )
-                    .then(Commands.literal("grant")
-                    )
-                    .then(Commands.literal("removal")
-                    )
+                    .then(Commands.literal("has"))
+                    .then(Commands.literal("grant"))
+                    .then(Commands.literal("removal"))
                 )
             )
         );
     }
 
     public static int listCurses(CommandSourceStack source) {
-        List<Curse> curses = CurseManager.getAllCurses();
-        return ; // so far i don't think this works
+        List<AbstractCurse> curses = CurseManager.getAllCurses();
+        return 0; // so far i don't think this works
+    }
+
+    private static int cast(CommandSourceStack pSource, Collection<? extends LivingEntity> pTargets) {
+        Iterator targets = pTargets.iterator();
+
+        while(targets.hasNext()) {
+            Curses.LIFEBANE.get().cast((LivingEntity) targets.next());
+        }
+
+        return pTargets.size();
     }
 }
